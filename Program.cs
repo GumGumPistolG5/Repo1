86654f0d43f4b32ff0863e3726d26371b9540360
Benjamin Nikou (1000114581) <NIKOB2@student.op.ac.nz>
2025-09-16 11:20:31 +1200

    Benjamin's cool stuff

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿namespace test
+﻿using System;
+
+namespace test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
+            Console.WriteLine("")
+            Console.WriteLine("Hello everyone!")
             Console.WriteLine("Im In!");
         }
     }
